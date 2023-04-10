76738e54e32a47a9aadcd048c4f99a1e4d6275de
Andrei Betlen <abetlen@gmail.com>
2023-04-10 14:38:25 -0400

    Initial commit

---
+++ ggml/__init__.py
@@ -0,0 +1,1 @@
+from ggml import *
---
+++ setup.py
@@ -0,0 +1,21 @@
+from skbuild import setup
+
+setup(
+    name="ggml_python",
+    description="A Python wrapper for ggml",
+    long_description_content_type="text/markdown",
+    version="0.0.1",
+    author="Andrei Betlen",
+    author_email="abetlen@gmail.com",
+    license="MIT",
+    packages=["ggml"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
