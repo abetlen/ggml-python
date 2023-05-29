6ee6aa2ef4e5553ab8654cb07c6eed8081f63934
Andrei Betlen <abetlen@gmail.com>
2023-05-29 12:49:51 -0400

    Add simple init test

--- tests/test_ggml.py
+++ tests/test_ggml.py
@@ -2,3 +2,8 @@ import ggml
 
 def test_ggml():
     assert ggml.GGML_FILE_VERSION.value == 1
+
+    params = ggml.ggml_init_params(0, None, False)
+    ctx = ggml.ggml_init(params=params)
+    assert ggml.ggml_used_mem(ctx) == 0
+    ggml.ggml_free(ctx)
