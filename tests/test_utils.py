aa4603496ce7ae7e2d3e450d8833e34e631378e1
Andrei Betlen <abetlen@gmail.com>
2023-06-30 03:40:09 -0400

    Simplify numpy util functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,3 +12,14 @@ def test_utils():
         assert t.contents.ne[:1] == [3]
         assert t.contents.type == ggml.GGML_TYPE_F32
         assert np.allclose(ggml.utils.to_numpy(t), x)
+
+def test_numpy_arrays():
+    params = ggml.ggml_init_params(mem_size=16 * 1024 * 1024)
+    with ggml.utils.ggml_context_manager(params) as ctx:
+        x = np.array([[1, 2, 3],
+                      [4, 5, 6]], dtype=np.float32, order='F')
+        assert x.shape == (2, 3)
+        t = ggml.utils.from_numpy(x, ctx)
+        assert t.contents.ne[:2] == [2, 3] # <- this breaks
+        y = ggml.utils.to_numpy(t)
+        assert y.shape == (2, 3)
