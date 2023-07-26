482c1113730c9d727a1aeeaeab015061b38a2373
Mohammadreza Anvari <mrezanvari@gmail.com>
2023-07-26 16:46:49 -0400

    Add GgmlBackendRep to load model graph and weights

--- ggml/contrib/onnx.py
+++ ggml/contrib/onnx.py
@@ -1,11 +1,121 @@
-import onnx
-from onnx.backend.base import Backend
+from typing import Any, Tuple
+
+from onnx import defs
+from onnx.backend.base import Backend, BackendRep
+from onnx.helper import make_opsetid
+from onnx.onnx_ml_pb2 import GraphProto
+
+
+class GgmlBackendRep(BackendRep):
+    def __init__(self, graph=None, inputs=None, outputs=None, tensor_dict=None):
+        super(GgmlRuntimeBackend, self).__init__()
+        self._graph = graph
+        self._inputs = inputs or {}
+        self._outputs = outputs or {}
+        self._tensor_dict = tensor_dict or {}
+
+    @property
+    def graph(self):
+        return self._graph
+
+    @graph.setter
+    def graph(self, graph):
+        self._graph = graph
+
+    @property
+    def inputs(self):
+        return self._inputs
+
+    @inputs.setter
+    def inputs(self, inputs):
+        self._inputs = inputs
+
+    @property
+    def outputs(self):
+        return self._outputs
+
+    @outputs.setter
+    def outputs(self, outputs):
+        self._outputs = outputs
+
+    @property
+    def tensor_dict(self):
+        return self._tensor_dict
+
+    @tensor_dict.setter
+    def tensor_dict(self, tensor_dict):
+        self._tensor_dict = tensor_dict
+
+    def run(self, inputs: Any, **kwargs: Any) -> Tuple[Any, ...]:
+        """Abstract function."""
+
+        # check where data is should be on CPU
+        return (None,)
 
 
 class GgmlRuntimeBackend(Backend):
     @classmethod
-    def prepare(cls, model, device=None, **kwargs):
-        pass
+    def is_opset_supported(cls, model):  # pylint: disable=unused-argument
+        return True, ""
+
+    @classmethod
+    def prepare(cls, model, device="CPU", **kwargs):
+        """
+        Load the model and creates a :class:`onnxruntime.InferenceSession`
+        ready to be used as a backend.
+
+        :param model: ModelProto (returned by `onnx.load`),
+            string for a filename or bytes for a serialized model
+        :param device: requested device for the computation,
+            None means the default one which depends on
+            the compilation settings
+        :param kwargs: see :class:`onnxruntime.SessionOptions`
+        :return: :class:`onnxruntime.InferenceSession`
+        """
+
+        super(GgmlRuntimeBackend, cls).prepare(model, device, **kwargs)
+        ggml_rep = cls.onnx_model_to_ggml_rep(model, **kwargs)
+
+        return ggml_rep
+
+    @classmethod
+    def onnx_model_to_ggml_rep(cls, model, **kwargs):
+        """Convert ONNX model to GgmlRep.
+
+        :param model: ONNX ModelProto object.
+        and the converted tensorflow model.
+        :return: GgmlRep object.
+        """
+
+        # Models with IR_VERSION less than 3 does not have opset_import set.
+        # We default to minimum opset, this behavior is consistent with
+        # onnx checker.
+        # c.f. https://github.com/onnx/onnx/blob/427ac0c1b792363d373e3d7e4eef97fa46458420/onnx/checker.cc#L478
+        if model.ir_version < 3:
+            opset_import = [make_opsetid(defs.ONNX_DOMAIN, 1)]
+        else:
+            opset_import = model.opset_import
+
+        return cls._onnx_graph_to_ggml_rep(model.graph, opset_import, **kwargs)
+
+    @classmethod
+    def _onnx_graph_to_ggml_rep(cls, graph_def: GraphProto, opset, **kwargs):
+        inputs = {}
+        outputs = {}
+        weights = {}
+
+        for node in graph_def.node:
+            inputs[node.name] = list(node.input)
+            outputs[node.name] = list(node.output)
+
+        for initializer in graph_def.initializer:
+            weights[initializer.name] = initializer.raw_data
+
+        # ggml_rep -> GgmlBackendRep() -> return
+
+        return GgmlBackendRep(
+            graph_def, inputs=inputs, outputs=outputs, tensor_dict=weights
+        )
 
     @classmethod
     def run_model(cls, model, inputs, device=None, **kwargs):
