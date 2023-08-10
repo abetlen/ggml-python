0e8cfa460a088c19bae2109123903a49403e71f5
Mohammadreza Anvari <mrezanvari@gmail.com>
2023-08-10 16:03:13 -0400

    Add Relu operator

--- ggml/contrib/onnx.py
+++ ggml/contrib/onnx.py
@@ -326,7 +326,21 @@ def ggml_operator_gather(
 def ggml_operator_relu(
     node: NodeProto, tensors_dict: dict, context: ggml.ggml_context_p, refs: List[Any]
 ):
-    raise NotImplementedError(f'Operator "Relu" not implemented')
+    node_inputs = [tensors_dict[inp] for inp in node.input]
+
+    if len(node_inputs) != 1:
+        raise ValueError(
+            f'Error for node "{node.name}": Operation "Relu" requires exactly one input. Actual number of inputs: {len(node_inputs)}'
+        )
+
+    output_name = node.output[0]
+
+    relu_result = ggml.ggml_relu(
+        context,
+        *node_inputs,
+    )
+    tensors_dict[output_name] = relu_result
+    return relu_result
 
 
 @ggml_operator("MatMul")
