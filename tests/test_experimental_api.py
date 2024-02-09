d88f325c3c28c3ce38679a500d7ffe954a730b6d
Andrei Betlen <abetlen@gmail.com>
2024-02-09 18:22:03 -0500

    Update high level api

--- tests/test_experimental_api.py
+++ tests/test_experimental_api.py
@@ -1,5 +1,6 @@
 from ggml.experimental import ggml_context, ggml_cgraph, Tensor, GGML_TYPE, Backend
 
+
 def test_experimental_api():
     backend = Backend.cpu()
 
@@ -59,34 +60,30 @@ def test_experimental_api():
     with ggml_context():
         a = Tensor.with_shape((1,), ggml_type=GGML_TYPE.F32)
         b = Tensor.with_shape((1,), ggml_type=GGML_TYPE.F32)
+        x = Tensor.with_shape((1,), ggml_type=GGML_TYPE.F32)
 
         backend.alloc_ctx_tensors()
 
         a[0] = 3.0
         b[0] = 4.0
 
-        with ggml_context():
-            x = Tensor.with_shape((1,), ggml_type=GGML_TYPE.F32)
+        f = a * x * x + b
 
-            backend.alloc_ctx_tensors()
+        assert f.shape == (1,)
 
-            x[0] = 2.0
+        measure_allocr = backend.new_measure()
 
-            f = a * x * x + b
+        graph = ggml_cgraph(f)
 
-            assert f.shape == (1,)
+        mem_size = measure_allocr.alloc_graph(graph)
 
-            measure_allocr = backend.new_measure()
+        buffer = backend.alloc_buffer(mem_size)
 
-            graph = ggml_cgraph(f)
+        allocr = buffer.new_allocr()
+        allocr.alloc_graph(graph)
 
-            mem_size = measure_allocr.alloc_graph(graph)
+        x[0] = 2.0
 
-            buffer = backend.alloc_buffer(mem_size)
+        graph.compute(backend)
 
-            allocr = buffer.new_allocr()
-            allocr.alloc_graph(graph)
-
-            graph.compute(backend)
-
-            assert f[0] == 16.0
+        assert f[0] == 16.0
